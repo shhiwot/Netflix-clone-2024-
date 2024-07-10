8d1ea91fa44bb5d7bda294b7db102022541aec00
shhiwot <shhiwot14@gmail.com>
2024-07-10 00:26:51 -0500

    Header and Footer

---
+++ Netflix-clone/src/Component/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import footerData from "./FooterData";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import "./footer.css";
+const Footer = () => {
+  return (
+    <div className="Footerouter_wrapper">
+      <div className="icon_Wrapper">
+        <FacebookIcon />
+        <InstagramIcon />
+        <TwitterIcon />
+        <YouTubeIcon />
+      </div>
+
+      <div className="footer_Wrapper row">
+        {footerData.map((obj, i) => {
+          let { url, title } = obj;
+          return (
+            <div className="colomun_list col-6 col-lg-3" key={i}>
+              <a href={url}>{title}</a>
+            </div>
+          );
+        })}
+      </div>
+      <div className="last_footerWrapper">
+        <div className="service_wrapper">Service Code</div>
+        <br />
+        <CopyrightIcon />
+        1997-2024 Netflix inc.
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ Netflix-clone/src/Component/Footer/FooterData.jsx
@@ -0,0 +1,19 @@
+const footerData = [
+  { url: "/", title: "Audio Description" },
+  { url: "/", title: "Gift Cards" },
+  { url: "/", title: "Investor Relations" },
+  { url: "/", title: "Netflix Shop" },
+  { url: "/", title: "Privacy" },
+  { url: "/", title: "Cookie Preferences" },
+  { url: "/", title: "Ad Choices" },
+  { url: "/", title: "Terms Of Use " },
+  { url: "/", title: "Help Center" },
+  { url: "/", title: "Media Center" },
+  { url: "/", title: "Jobs" },
+  { url: "/", title: "Contact Us " },
+  { url: "/", title: "Legal Notices" },
+  { url: "/", title: "Corporate Information" },
+  { url: "/", title: "DoNot Sell or Share My Personal\n Information" },
+];
+
+export default footerData;
